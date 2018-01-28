13ccc4f70948a3076b99a0fd5a69ec84b9a3d655
leewei05 <B10202013@mail.ntust.edu.tw>
2018-01-28 13:06:41 +0800

    arrays

---
+++ README.md
@@ -0,0 +1,11 @@
+## Data Structure in Python
+
+# Arrays 
+----------
+
+A collection of values each identified by an index.
+
+* index starts at zero
+* random access is possible if you know the index of the item
+
+**Advantages**
---
+++ arrays.py
@@ -0,0 +1,28 @@
+
+numbers = [1,2,3,4,5]
+
+# random indexing --> O(1) get items if we know the index !!
+# print(numbers[0])
+
+# for loop
+# for num in numbers:
+#     print(num)
+
+# for sequence
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+# [start index : end index]
+print(numbers[0:2])
+
+# [:] all fo the items
+print(numbers[:])
+
+# O(N) search running time
+maximum = numbers[0]
+for num in numbers:
+    if num > maximum:
+        maximum = num
+
+print(maximum)
+
